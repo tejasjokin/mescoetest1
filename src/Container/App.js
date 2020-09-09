489886aa1283d30d61919430119e4da09f3d1dc8
tanmayjagtap <tanmayjagtap27@gmail.com>
2020-09-09 12:52:20 +0530

    forked

--- src/Container/App.js
+++ src/Container/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Card from '../Components/Card/Card.js';
 import Navigation from '../Components/Navigation/Navigation.js';
 import SignIn from '../Components/SignIn/SignIn.js';
+import Home from '../Components/Home/Home.js';
 import './App.css';
 import StudentImage from '../Images/StudentImage.png';
 import TeacherImage from '../Images/TeacherImage.png';
@@ -61,6 +62,14 @@ class App extends Component{
         <div className="App">
           <Navigation onRouteChange = {this.onRouteChange}/>
           <SignIn user_type ={type} />
+        </div>
+      );
+    }
+    else if(route==='Home'){
+      return(
+        <div className="App">
+          <Navigation onRouteChange = {this.onRouteChange}/>
+          <Home />
         </div>
       );
     }
